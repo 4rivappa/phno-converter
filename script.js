7da81bb7bc281ee2c7860172d6d5c11e6fa2117c
4rivappa <arivappa21@gmail.com>
2022-10-08 18:50:51 +0530

    project backup

---
+++ script.js
@@ -0,0 +1,118 @@
+
+const arr = ["you", "I", "to", "the", "and", "that", "of", "me", "in", "this", "for", "no", "have", "my", "just", "not", "do", "be", "on", "your", "was", "we", "with", "but", "all", "well", "he", "about", "right", "get", "here", "out", "if", "her", "she", "can", "up", "want", "now", "go", "him", "there", "one", "why", "see", "come", "good", "they", "really", "when", "back", "from", "were", "yes", "his", "or", "who", "because", "some", "then", "say", "an", "way", "us", "little", "never", "too", "sure", "more", "over", "our", "sorry", "where", "let", "maybe", "down", "man", "very", "by", "anything", "much", "any", "life", "even", "off", "please", "thank", "only", "help", "two", "talk", "people", "still", "wait", "into", "find", "again", "thing", "call", "told", "great", "before", "better", "ever", "night", "than", "away", "first", "believe", "fine", "home", "after", "last", "these", "put", "around", "stop", "long", "always", "listen", "those", "big", "lot", "kind", "wrong", "through", "new", "guess", "care", "bad", "remember", "together", "dad", "leave", "mother", "place", "understand", "actually", "hear", "baby", "nice", "father", "else", "stay", "their", "course", "might", "mind", "every", "enough", "try", "hell", "came", "someone", "own", "family", "whole", "another", "house", "yourself", "idea", "ask", "best", "must", "old", "woman", "hello", "which", "room", "money", "left", "tonight", "real", "son", "hope", "name", "same", "happy", "pretty", "girl", "sir", "show", "already", "may", "next", "three", "found", "world", "honey", "myself", "exactly", "probably", "hurt", "both", "while", "dead", "alone", "since", "excuse", "start", "kill", "hard", "today", "car", "ready", "until", "without", "whatever", "deal", "took", "once", "friend", "head", "stuff", "most", "worry", "second", "part", "truth", "school", "face", "forget", "business", "each", "cause", "soon", "wife", "use", "chance", "run", "move", "anyone", "person", "bye", "somebody", "heart", "such", "point", "later", "meet", "anyway", "phone", "reason", "lost", "look", "bring", "case", "wish", "tomorrow", "trust", "check", "end", "late", "anymore", "five", "least", "town", "year", "make", "mean", "play", "hate", "ago", "beautiful", "fact", "crazy", "party", "sit", "open", "afraid", "between", "important", "rest", "fun", "kid", "glad", "everyone", "day", "sister", "minute", "everybody", "bit", "couple", "either", "daughter", "under", "break", "door", "set", "close", "easy", "doctor", "far", "walk", "need", "trouble", "mine", "though", "time", "different", "hospital", "anybody", "alright", "wedding", "shut", "able", "die", "perfect", "police", "stand", "hit", "story", "dinner", "against", "funny", "husband", "almost", "stupid", "pay", "answer", "four", "office", "cool", "news", "child", "half", "moment", "sleep", "young", "men", "sonny", "lucky", "pick", "them", "bed", "also", "date", "line", "lose", "fire", "free", "hand", "serious", "shit", "behind", "inside", "high", "ahead", "wonderful", "fight", "past", "cut", "quite", "number", "sick", "game", "eat", "nobody", "death", "along", "finally", "upset", "seem", "safe", "children", "front", "shot", "love", "clear", "six", "drink", "absolutely", "how", "sweet", "alive", "sense", "happen", "special", "bet", "blood", "lie", "full", "meeting", "dear", "coffee", "sound", "fault", "water", "ten", "women", "welcome", "buy", "hour", "speak", "think", "body", "order", "outside", "hang", "worse", "company", "mistake", "handle", "spend", "totally", "control", "marriage", "power", "president", "unless", "send", "picture", "hundred", "change", "explain", "certainly", "sign", "boy", "relationship", "hair", "choice", "anywhere", "secret", "future", "weird", "luck", "touch", "crane", "question", "obviously", "pain", "straight", "grace", "white", "fast", "word", "food", "none", "drive", "work", "marry", "light", "test", "drop", "frank", "sent", "city", "dream", "protect", "twenty", "class", "surprise", "forever", "poor", "mad", "except", "gun", "know", "dance", "take", "especially", "situation", "besides", "week", "himself", "act", "worth", "top", "expect", "rather", "involve", "swear", "piece", "busy", "law", "black", "movie", "catch", "country", "less", "perhaps", "step", "fall", "dog", "win", "personal", "admit", "problem", "strong", "evil", "feel", "honest", "eye", "broke", "miss", "tired", "evening", "human", "red", "entire", "trip", "club", "suppose", "calm", "imagine", "fair", "blame", "street", "apartment", "court", "terrible", "clean", "learn", "relax", "million", "charity", "accident", "prove", "smart", "message", "small", "interest", "table", "become", "mouth", "pregnant", "middle", "ring", "careful", "shall", "team", "ride", "figure", "wear", "shoot", "stick", "ray", "follow", "angry", "instead", "buddy", "write", "early", "angel", "nick", "ran", "war", "forgive", "jail", "lunch", "eight", "thousand", "music", "tough", "tape", "count", "college", "proud", "agree", "birthday", "bill", "seven", "history", "share", "offer", "hurry", "feet", "wonder", "simple", "decision", "building", "finish", "voice", "herself", "would", "list", "mess", "deserve", "cute", "dress", "interesting", "hotel", "enjoy", "quiet", "road", "eve", "short", "beat", "mention", "neither", "fix", "respect", "spent", "prison", "attention", "near", "bar", "pass", "gift", "dark", "self", "normal", "aunt", "dollar", "lawyer", "apart", "certain", "plan", "floor", "whether", "everything", "present", "earth", "private", "box", "cover", "judge", "sake", "possibly", "worst", "station", "accept", "blow", "strange", "save", "plane", "yesterday", "quick", "lately", "stuck", "lovely", "security", "report", "difference", "store", "bag", "ball", "single", "doubt", "blue", "deep", "park", "record", "lord", "join", "key", "captain", "card", "crime", "window", "return", "guilty", "difficult", "soul", "joke", "service", "magic", "uncle", "promise", "public", "bother", "island", "seriously", "cell", "lead", "broken", "advice", "somehow", "push", "concern", "usually", "boss", "rule", "summer", "thirty", "risk", "letting", "officer", "support", "afternoon", "born", "seat", "nervous", "across", "song", "charge", "patient", "boat", "brain", "hide", "general", "nine", "huge", "breakfast", "horrible", "age", "awful", "pleasure", "driving", "system", "sell", "quit", "dying", "chief", "faith", "month", "visit", "screw", "letter", "decide", "double", "sad", "press", "forward", "fool", "smell", "spell", "memory", "mark", "slow", "hungry", "board", "position", "hearing", "rose", "kitchen", "force", "fly", "during", "space", "kick", "other", "grab", "discuss", "third", "cat", "fifty", "mile", "fat", "reading", "idiot", "rock", "suddenly", "agent", "bunch", "destroy", "track", "shoes", "scene", "peace", "demon", "low", "consider", "drunk", "tell", "knock", "bell", "cash", "give", "department", "nose", "turn", "keep", "beer", "sooner", "plenty", "extra", "attack", "ground", "whose", "weekend", "matter", "wrote", "type", "opportunity", "king", "impossible", "book", "machine", "waste", "pretend", "danger", "wall", "jump", "proof", "complete", "arrest", "perfectly", "warm", "pull", "twice", "easier", "suit", "comfortable", "fit", "begin", "closely", "although", "smile", "laugh", "fish", "treat", "fear", "guy", "otherwise", "excited", "mail", "green", "stole", "notice", "excellent", "pop", "paper", "bottom", "note", "sudden", "bathroom", "sing", "glass", "tree", "contact", "shop", "reach", "cry", "cake", "partner", "bus", "computer", "study", "star", "area", "wind", "chicken", "dry", "hero", "error", "are", "build", "sea", "Saturday", "add", "birth", "bird", "grandmother", "heavy", "west", "lesson", "heat", "speed", "milk", "rain", "sugar", "market", "process", "stone", "serve", "river", "super", "monkey", "chat", "signal", "cup", "bee", "a", "above", "addition", "among", "amount", "angle", "animal", "appear", "apple", "art", "at", "bank", "base", "bear", "belong", "block", "bone", "brown", "cannot", "capital", "carry", "centre", "century", "circle", "cloud", "cost", "cold", "column", "common", "condition", "contain", "continue", "cook", "copy", "cow", "create", "cross", "crowd", "describe", "desert", "design", "detail", "dictionary", "direction", "ear", "east", "like", "edge", "effect", "egg", "element", "energy", "engine", "example", "express", "famous", "farm", "field", "finger", "flat", "flower", "foot", "forest", "form", "fresh", "fruit", "garden", "gas", "gold", "group", "grow", "hat", "hole", "horse", "hunt", "ice", "inch", "include", "increase", "indicate", "information", "insect", "instrument", "iron", "is", "itself", "job", "lady", "lake", "land", "language", "large", "lay", "leg", "length", "level", "lift", "main", "many", "map", "march", "gap", "pan", "read", "made", "morning", "okay", "rid", "defense", "planning", "flight", "honestly", "remind", "witness", "dare", "hardly", "steal", "princess", "silly", "teach", "plus", "trial", "roll", "radio", "dirty", "choose", "emergency", "credit", "obvious", "positive", "nut", "price", "goodbye", "guard", "mood", "total", "crap"];
+
+document.getElementById("form").addEventListener("submit", converter);
+document.getElementById("secondForm").addEventListener("submit", reConverter);
+
+function converter(e){
+    e.preventDefault(); 
+    var number = Number(document.getElementById("phno").value);
+    var dArr = new Array(10);
+    for(var i=9; i>=0; i--){ 
+        dArr[i] = number%10;
+        number = Math.floor(number/10);
+    }
+    var firstNum = dArr[1]*100 + dArr[2]*10 + dArr[3];
+    var secondNum = dArr[4]*100 + dArr[5]*10 + dArr[6];
+    var thirdNum = dArr[7]*100 + dArr[8]*10 + dArr[9];
+    document.getElementById("code").innerText = `${dArr[0]}.${arr[firstNum]}.${arr[secondNum]}.${arr[thirdNum]}`;
+}
+
+function reConverter(e){
+    e.preventDefault();
+    var number = Number(document.getElementById("startNum").value);
+    var firstName = document.getElementById("firstName").value;
+    var secondName = document.getElementById("secondName").value;
+    var thirdName = document.getElementById("thirdName").value;
+    var firstNum = arr.indexOf(firstName);
+    var secondNum = arr.indexOf(secondName);
+    var thirdNum = arr.indexOf(thirdName);
+    if(firstNum!=-1 && secondNum!=-1 && thirdNum!=-1){ 
+        if(firstNum<10) firstName = `00${firstNum}`;
+        else if(firstNum<100) firstName = `0${firstNum}`;
+        else firstName = `${firstNum}`;
+
+        if(secondNum<10) secondName = `00${secondNum}`;
+        else if(secondNum<100) secondName = `0${secondNum}`;
+        else secondName = `${secondNum}`;
+
+        if(thirdNum<10) thirdName = `00${thirdNum}`;
+        else if(thirdNum<100) thirdName = `0${thirdNum}`;
+        else thirdName = `${thirdNum}`;
+
+        document.getElementById("reCode").innerText = `${number}${firstName}${secondName}${thirdName}`;
+    }else{ 
+        document.getElementById("reCode").innerText = `Enter correct words`;
+    }
+}
+
+
+function autocomplete(elem, arr){ 
+    var currentFocus;
+    elem.addEventListener("input", function(e){
+        var val = this.value; 
+        closeAllLists();
+        if(!val){ return false;}
+        currentFocus = -1;
+        var divBox = document.createElement("div");
+        divBox.setAttribute("id", this.id+"autocompleteList");
+        divBox.setAttribute("class", "autocompleteItems");
+        console.log(this.parentNode);
+        this.parentNode.appendChild(divBox);
+        for(let i=0; i<arr.length; i++){ 
+            if(arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()){ 
+                var optionDiv = document.createElement("div");
+                optionDiv.setAttribute("class", "listItem");
+                optionDiv.innerHTML = "<strong>"+arr[i].substr(0, val.length)+"</strong>";
+                optionDiv.innerHTML += arr[i].substr(val.length, arr[i].length);
+                optionDiv.innerHTML += `<input type='hidden' value='${arr[i]}'>`;
+                optionDiv.addEventListener("click", function(e){ 
+                    elem.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                })
+                divBox.appendChild(optionDiv);
+            }
+        }
+    })
+    elem.addEventListener("keydown", function(e){ 
+        var x = document.getElementById(this.id + "autocompleteList");
+        if(x) x = x.getElementsByTagName("div");
+        if(e.keyCode == 40){ 
+            currentFocus++;
+            makeActive(x);
+        }else if(e.keyCode == 38){ 
+            currentFocus--;
+            makeActive(x);
+        }else if(e.keyCode == 13){ 
+            e.preventDefault();
+            if(currentFocus > -1){ 
+                if(x) x[currentFocus].click();
+            }
+        }
+    })
+    function makeActive(x){ 
+        if(!x) return false;
+        removeActive(x);
+        if(currentFocus >= x.length) currentFocus=0;
+        if(currentFocus < 0) currentFocus = (x.length-1);
+        x[currentFocus].classList.add("autocompleteActive");
+    }
+    function removeActive(x){ 
+        for(var i=0; i<x.length; i++)
+            x[i].classList.remove("autocompleteActive");
+    }
+    function closeAllLists(element){ 
+        var items = document.getElementsByClassName("autocompleteItems");
+        for(var i=0; i<items.length; i++){ 
+            if(element!=items[i] && element!=elem){ 
+                items[i].parentNode.removeChild(items[i]);
+            }
+        }
+    }
+    document.addEventListener("click", function(e){ 
+        closeAllLists(e.target);
+    })
+}
+autocomplete(document.getElementById("firstName"), arr);
+autocomplete(document.getElementById("secondName"), arr);
+autocomplete(document.getElementById("thirdName"), arr);
